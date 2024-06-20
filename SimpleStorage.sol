3bf559905966e14caab8b413050585c6afafa2b2
Afrem05 <zweibis5plus0@gmail.com>
2024-06-20 08:52:22 +0200

    Remix Storage Factory

---
+++ SimpleStorage.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract SimpleStorage {
+
+    uint256 public favoriteNumber;
+
+    // Dictionary where every single name maps to a specific number
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    People[] public people;
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // Change the value of favoriteNumber
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    // View and pure functions don't spend gas
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
+
+    // Structs, mappings, and arrays are memory when added to parameters
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
